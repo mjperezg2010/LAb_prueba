ad5fdc729c605619479b0fcfeddb1055527a1e54
MartinPerez <mjperezg2010@hotmail.com>
2017-10-30 20:43:33 -0600

    Deje de joder

--- LAbsimulacionelfos/src/Bestia/Aguilas.java
+++ LAbsimulacionelfos/src/Bestia/Aguilas.java
@@ -9,6 +9,25 @@ package Bestia;
  *
  * @author mjper
  */
-public class Aguilas {
+public class Aguilas extends Bestia{
+    private String colorplumaje;
+
+    public Aguilas() {
+    }
+
+    public Aguilas(String colorplumaje, int garras, boolean veneno) {
+        super(garras, veneno);
+        this.colorplumaje = colorplumaje;
+    }
+
+    public String getColorplumaje() {
+        return colorplumaje;
+    }
+
+    public void setColorplumaje(String colorplumaje) {
+        this.colorplumaje = colorplumaje;
+    }
+    
+    
     
 }
